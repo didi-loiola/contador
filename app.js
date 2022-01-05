d038307cc9f3189e1a6f6f6dbc3fd9b4d18e5aeb
Diego Loiola <diego.alves_1996@hotmail.com>
2022-01-05 10:43:51 -0300

    Adicionando limites ao contador

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var currentNumberWrapper = document.getElementById('currentNumber')
 var currentNumber = 0
 
 function increment() {
-    currentNumber += 1
-    currentNumberWrapper.innerHTML = currentNumber
+    if(currentNumber < 10){
+        currentNumber += 1
+        currentNumberWrapper.innerHTML = currentNumber
+    }
 }
 
 function decrement() {
-    currentNumber -= 1
-    currentNumberWrapper.innerHTML = currentNumber
+    if(currentNumber > 0){
+        currentNumber -= 1
+        currentNumberWrapper.innerHTML = currentNumber
+    }
 }
 
-document.getElementById('adicionar').addEventListener('click', increment)
 document.getElementById('subtrair').addEventListener('click', decrement)
+document.getElementById('adicionar').addEventListener('click', increment)
